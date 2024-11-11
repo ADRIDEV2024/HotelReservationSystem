f32aa2bd16f7e7a5f631269c67a237296d4918f1
Adrian <adriangomis34@gmail.com>
2024-11-11 13:35:57 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,12 @@
 document.getElementById('google-login').addEventListener('click', () => {
     window.location.href = 'http://localhost:3000/auth/google';
   });
-  
+
+function showError(message) {
+  const alertBox = document.createElement('div');
+  alertBox.className = 'alert alert-danger';
+  alertBox.textContent = message;
+  document.body.prepend(alertBox);
+
+  setTimeout(() => alertBox.remove(), 5000); // Eliminar alerta después de 5 segundos
+}
