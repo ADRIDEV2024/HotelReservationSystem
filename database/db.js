9c6cd3e2addf211ac09bd1bc55a3ad53d918a36f
Adrian <adriangomis34@gmail.com>
2024-11-13 08:24:34 +0100

    Updated some files

---
+++ database/db.js
@@ -0,0 +1,45 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  storage: './database/hotel.mysql',
+});
+
+const db = {};
+
+db.Room = sequelize.define('Room', {
+  number: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+db.Reservation = sequelize.define('Reservation', {
+  guestName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  checkIn: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  checkOut: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  roomId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: 'Room',
+      key: 'id',
+    },
+    allowNull: false,
+  },
+});
+
+export default { sequelize };
