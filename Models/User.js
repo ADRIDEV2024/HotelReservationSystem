e01465e0dee5982d2209c1e712fab8d5e41e5281
Adrian <adriangomis34@gmail.com>
2024-11-13 08:14:03 +0100

    Modelo de usuario revisado y mejorado

---
+++ Models/User.js
@@ -0,0 +1,34 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/config';
+
+const User = sequelize.define('User', {
+  googleId: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  mfaSecret: {
+    type: DataTypes.STRING,
+  },
+  isMfaEnabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+const UserModel = {
+  User,
+  create: async (googleId, name) => {
+    const user = await User.create({ googleId, name });
+    return user;
+  },
+};
+
+function getUserByGoogleId(googleId) {
+  return User.findOne({ where: { googleId } });
+}
+
+export { UserModel, getUserByGoogleId };
+export default User;
