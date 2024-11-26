a0891044c546a6356a9be3759fca7e27a3a4974f
Adrian <adriangomis34@gmail.com>
2024-11-26 09:13:03 +0100

    Updated routes

--- roomRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllRooms, createRoom } from '../controllers/roomController.js';
-
-const router = Router();
-
-router.get('/', getAllRooms);
-router.post('/', createRoom);
-
-export default router;
