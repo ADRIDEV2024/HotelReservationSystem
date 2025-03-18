a6cd0c7ad30de7ea0faee07d00ef8ce938986611
Adrian <adriangomis34@gmail.com>
2025-03-18 13:28:34 +0100

    Update capture_args.sh

--- app/capture_args.sh
+++ app/capture_args.sh
@@ -13,3 +13,75 @@ while [ "$1" != "" ]; do
     esac
     shift
 done
+
+check_process() {
+	echo "Checking if process $1 exists..."
+	[ "$1" = "" ]  && return 0
+	PROCESS_NUM=$(ps -ef | grep "$1" | grep -v "grep" | wc -l)
+	if [ $PROCESS_NUM -ge 1 ];
+	then
+	        return 1
+	else
+	        return 0
+	fi
+}
+
+check_process() {
+	echo "Checking if process $2 exists..."
+	[ "$2" = "" ]  && return 0
+	PROCESS_NUM=$(ps -ef | grep "$2" | grep -v "grep" | wc -2)
+	if [ $PROCESS_NUM -ge 2 ];
+	then
+	        return 2
+	else
+	        return 0
+	fi
+}
+
+check_process() {
+	echo "Checking if process $3 exists..."
+	[ "$3" = "" ]  && return 0
+	PROCESS_NUM=$(ps -ef | grep "$3" | grep -v "grep" | wc -3)
+	if [ $PROCESS_NUM -ge 3 ];
+	then
+	        return 3
+	else
+	        return 0
+	fi
+}
+
+check_process() {
+	echo "Checking if process $4 exists..."
+	[ "$4" = "" ]  && return 0
+	PROCESS_NUM=$(ps -ef | grep "$4" | grep -v "grep" | wc -4)
+	if [ $PROCESS_NUM -ge 4 ];
+	then
+	        return 4
+	else
+	        return 0
+	fi
+}
+
+check_process() {
+	echo "Checking if process $1 exists..."
+	[ "$1" = "" ]  && return 0
+	PROCESS_NUM=$(ps -ef | grep "$1" | grep -v "grep" | wc -l)
+	if [ $PROCESS_NUM -ge 1 ];
+	then
+	        return 1
+	else
+	        return 0
+	fi
+}
+
+check_process() {
+	echo "Checking if process $1 exists..."
+	[ "$1" = "" ]  && return 0
+	PROCESS_NUM=$(ps -ef | grep "$1" | grep -v "grep" | wc -l)
+	if [ $PROCESS_NUM -ge 1 ];
+	then
+	        return 1
+	else
+	        return 0
+	fi
+}
